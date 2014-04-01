21afe7fa68e9e92f41d11ab7259e610d95564e45
Ferios <ExFerios@Gmail.com>
2014-04-02 02:53:53 +0400

    Table Onhover & Checkbox

--- v3/app/assets/javascripts/getFine.js
+++ v3/app/assets/javascripts/getFine.js
@@ -10,10 +10,14 @@ $(function() {
         if ($(row).hasClass('active') != true) {
             row.addClass('active');
             var s = this.cells[2].innerHTML;
+//            for (var j = 0, col; col = this.cells[j]; j++) {
+//                this.cells[j].style.backgroundColor='#f00'
+//            }
             var number = s.match(/\d+$/);
             number = parseInt(number, 10);
             sum += number;
             document.getElementById("total").innerHTML= sum.toString();
+            this.cells[0].getElementsByTagName('input')[0].checked = true;
         } else {
             row.removeClass('active');
             var s = this.cells[2].innerHTML;
@@ -21,8 +25,11 @@ $(function() {
             number = parseInt(number, 10);
             sum -= number;
             document.getElementById("total").innerHTML= sum.toString();
+            this.cells[0].getElementsByTagName('input')[0].checked = false;
         }
     });
+
+
 
     $(document).bind('selectstart dragstart', function(e) {
         e.preventDefault(); return false;
