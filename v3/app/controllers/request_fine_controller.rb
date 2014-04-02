b7a204f11d45ad77d9d0fb6df5df4f312b9b811c
Ferios <ExFerios@Gmail.com>
2014-04-03 03:30:03 +0400

    removed test link

--- v3/app/controllers/request_fine_controller.rb
+++ v3/app/controllers/request_fine_controller.rb
@@ -4,14 +4,14 @@ class RequestFineController < ApplicationController
   require 'json'
 
   def getFine
-    @auto_number = params[:auto_number]
-    @region = params[:region]
-    @driver_license = params[:driver_license]
-    @registration = params[:registration]
-    @registration_full = params[:registration_full]
+    #@auto_number = params[:auto_number]
+    #@region = params[:region]
+    #@driver_license = params[:driver_license]
+    #@registration = params[:registration]
+    #@registration_full = params[:registration_full]
 
     #uri = URI('http://www.dev.kibicom.com/test.json')
-    #    if @region != '16' #У Татарстана нет номера лицензии и регистрационный номер другой
+    #    if params[:region] != '16' #У Татарстана нет номера лицензии и регистрационный номер другой
     #      resp = Net::HTTP.post_form(uri, 'auto_number' => params[:auto_number], 'region' => params[:region], 'license' => params[:driver_license], 'reg' => params[:registration_full])
     #    else
     #      resp = Net::HTTP.post_form(uri, 'auto_number' => params[:auto_number], 'region' => params[:region], 'reg' => params[:registration])
