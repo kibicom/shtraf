0e369c13dd51207c6ae65cbafcf4909e0d8f9e50
sneg0vik <sneg0vik731@gmail.com>
2014-04-03 19:31:27 +0400

    fix conflict

--- v3/app/assets/javascripts/getFine.js
+++ v3/app/assets/javascripts/getFine.js
@@ -19,6 +19,7 @@ $(function() {
             sum += number;
             document.getElementById("total").innerHTML= sum.toString();
             this.cells[0].getElementsByTagName('input')[0].checked = true;
+            document.getElementById("payBtn").innerHTML="<iframe width=\"168\" height=\"80\" style=\"border:0;width:168px;height:80px;overflow:hidden;background-color:transparent;\" allowTransparency=\"true\" src=\"https://auth.robokassa.ru:443/Merchant/PaymentForm/FormV.if?MrchLogin=demo&OutSum="+sum+"&InvId=0&Desc=ROBOKASSA+Advanced+User+Guide&Shp_code=Asp&Shp_item=&SignatureValue=1c980c5b65d4e60b0f395ea2ddf6ed16&Culture=ru&Encoding=utf-8\"></iframe>";
         } else {
             row.removeClass('active');
             var s = this.cells[2].innerHTML;
@@ -27,6 +28,10 @@ $(function() {
             sum -= number;
             document.getElementById("total").innerHTML= sum.toString();
             this.cells[0].getElementsByTagName('input')[0].checked = false;
+            if(sum<=0)
+                document.getElementById("payBtn").innerHTML="<h4>Выберите штрафы для оплаты.</h4>";
+            else
+                document.getElementById("payBtn").innerHTML="<div style=\"margin: 0 auto; width: 168\"><iframe width=\"168\" height=\"80\" style=\"border:0;width:168px;height:80px;overflow:hidden;background-color:transparent;\" allowTransparency=\"true\" src=\"https://auth.robokassa.ru:443/Merchant/PaymentForm/FormV.if?MrchLogin=demo&OutSum="+sum+"&InvId=0&Desc=ROBOKASSA+Advanced+User+Guide&Shp_code=Asp&Shp_item=&SignatureValue=1c980c5b65d4e60b0f395ea2ddf6ed16&Culture=ru&Encoding=utf-8\"></iframe></div>"
         }
     });
 
